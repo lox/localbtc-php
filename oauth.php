88dedfdd6856d56ac3f71526f42912f50b073fc3
Lachlan Donald <lachlan@ljd.cc>
2013-11-02 19:09:42 +1100

    Fixed oauth flow

--- oauth.php
+++ oauth.php
@@ -18,7 +18,7 @@ $client = \LocalBtc\Authenticator::factory(array(
 if(isset($argv) && in_array('--authorize', $argv)) {
     printf("Visit and authorize app:\n%s\n\n", $client->authorizeUrl(array(
         'response_type' => 'code',
-        'scope' => 'read write',
+        'scope' => empty($argv[2]) ? 'read write' : $argv[2],
         'redirect_uri' => 'http://localhost:9000/',
     )));
 
@@ -34,8 +34,8 @@ parse_str($_SERVER['QUERY_STRING'], $query);
 
 // get an access token
 $token = $client->accessToken(array(
-    'code' => $query['code'],
     'grant_type' => 'authorization_code',
+    'code' => $query['code'],
     'client_id' => $client->getConfig('client_id'),
     'client_secret' => $client->getConfig('client_secret'),
 ));
@@ -45,10 +45,11 @@ $expires = date('r', time()+$token['expires_in']);
 
 echo <<<EOT
 <body>
-<h1>Access Token for <a href="http://localbitcoins.com">localbitcoins.com<a/></h1>
-<p style="font-size: 18px; font-weight: bold">
-{$token['access_token']}
-</p>
+<h1>API credentials for <a href="http://localbitcoins.com">localbitcoins.com<a/></h1>
+<dl style="font-size: 18px; font-weight: bold">
+<dt>Access Token</dt><dd>{$token['access_token']}</dd>
+<dt>Refresh Token</dt><dd>{$token['refresh_token']}</dd>
+</dl>
 <p>Expires on $expires</p>
 </body>
 EOT;
